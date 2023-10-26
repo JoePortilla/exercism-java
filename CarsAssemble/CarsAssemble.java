0746d7608b3070a3655fbce79a7782e249fde1f7
Joseph Santiago Portilla <37479719+JoePortilla@users.noreply.github.com>
2023-10-26 09:15:35 -0500

    iniciando carassemble

--- CarsAssemble/CarsAssemble.java
+++ CarsAssemble/CarsAssemble.java
@@ -1,9 +1,20 @@
 public class CarsAssemble {
+    public static void main(String[] args) {
+        System.out.println(productionRatePerHour(0));
+        System.out.println(workingItemsPerMinute(0));
+    }
+
     public static double productionRatePerHour(int speed) {
+        double rate = 0;
+
+        return rate;
 
     }
 
     public static int workingItemsPerMinute(int speed) {
+        int items = 0;
+
+        return items;
 
     }
 
