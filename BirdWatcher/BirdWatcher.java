50434a6e149a4d456be9c7fcdda6d0d97787d959
Joseph Santiago Portilla <37479719+JoePortilla@users.noreply.github.com>
2023-10-26 21:10:46 -0500

    triangle

--- BirdWatcher/BirdWatcher.java
+++ BirdWatcher/BirdWatcher.java
@@ -1,6 +1,29 @@
 package BirdWatcher;
 
 public class BirdWatcher {
+    private final int[] birdsPerDay;
+
+    public BirdWatcher(int[] birdsPerDay) {
+        this.birdsPerDay = birdsPerDay.clone();
+    }
+
+    public int[] getLastWeek() {
+    }
+
+    public int getToday() {
+    }
+
+    public void incrementTodaysCount() {
+    }
+
+    public boolean hasDayWithoutBirds() {
+    }
+
+    public int getCountForFirstDays(int numberOfDays) {
+    }
+
+    public int getBusyDays() {
+    }
     public static void main(String[] args) {
         System.out.println();
     }
---
+++ Triangle/TriangleException.java
@@ -0,0 +1,7 @@
+package Triangle;
+
+public class TriangleException extends Exception {
+    public TriangleException() {
+        super();
+    }
+}
