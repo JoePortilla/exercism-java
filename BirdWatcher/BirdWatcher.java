cb1d7ef2f44262cc94aeb23f80bda4e0562b440e
Joseph Santiago Portilla <37479719+JoePortilla@users.noreply.github.com>
2023-10-27 05:42:03 -0500

    inicio de bird

--- BirdWatcher/BirdWatcher.java
+++ BirdWatcher/BirdWatcher.java
@@ -8,24 +8,35 @@ public class BirdWatcher {
     }
 
     public int[] getLastWeek() {
+        return {0,1};
+
     }
 
     public int getToday() {
+        return 0;
     }
 
     public void incrementTodaysCount() {
     }
 
     public boolean hasDayWithoutBirds() {
+        return false;
+
     }
 
     public int getCountForFirstDays(int numberOfDays) {
+        return 0;
+
     }
 
     public int getBusyDays() {
+        return 0;
+
     }
+
     public static void main(String[] args) {
-        System.out.println();
+        BirdWatcher watcher = new BirdWatcher(new int[] { 2, 3, 4, 5 });
+        System.out.println(watcher.getLastWeek());
     }
 
 }
