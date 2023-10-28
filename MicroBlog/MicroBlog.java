832b02dc43d5eefa9846e648ca60fd67dac8a723
Joseph Santiago Portilla <37479719+JoePortilla@users.noreply.github.com>
2023-10-27 23:18:46 -0500

    microblog

--- MicroBlog/MicroBlog.java
+++ MicroBlog/MicroBlog.java
@@ -1,15 +1,17 @@
 public class MicroBlog {
-    public static void main(String[] args) {
-        System.out.println(truncate("Transversalidad es una palabra larga"));
-    }
 
-    public static String truncate(String inputString) {
+    public String truncate(String inputString) {
         String truncatedString = "";
         if (inputString.length() > 5) {
-            truncatedString = inputString.substring(0, 5);
+            truncatedString = inputString.substring(0, inputString.offsetByCodePoints(0, 5));
         } else {
             truncatedString = inputString;
         }
         return truncatedString;
+    }
+
+    public static void main(String[] args) {
+        MicroBlog blog = new MicroBlog();
+        System.out.println(blog.truncate("Transversalidad es una palabra larga"));
     }
 }
